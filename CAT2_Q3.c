d577b1899ff8d71634d7d96ff0de0870cb45f3db
Owen634 <muthomiowen6@gmail.com>
2024-11-07 22:29:24 +0300

    Add files via upload

---
+++ CAT2_Q3.c
@@ -0,0 +1,51 @@
+//cat;gross pay, taxes and net pay program
+
+
+
+/*
+Author:MURITHI OWEN MUTHOMI
+Reg No:CT101/G/23216/24
+Date:7/11/2024
+*/
+
+
+
+#include <stdio.h>
+
+
+
+int main(){
+	float hoursWorked,hourlyWage,grossPay,taxes,netPay,overtimeHours;
+	
+	printf("Enter hours worked in a week:");
+	scanf("%f",&hoursWorked);
+	
+	printf("Enter hourly wage:");
+	scanf("%f",&hourlyWage);
+	
+	if(hoursWorked>40){
+		overtimeHours=hoursWorked-40;
+		grossPay=(40*hourlyWage)+(overtimeHours*hourlyWage*1.5);
+	}
+	else{
+		grossPay=hoursWorked*hourlyWage;
+	}
+	
+	if(grossPay<=600){
+		taxes=grossPay*0.15;
+	}
+	else{
+		taxes=(600*0.15)+((grossPay-600)*0.20);
+	}
+	netPay=grossPay-taxes;
+	
+	printf("Gross pay:$%.2f\n",grossPay);
+	printf("Taxes:$%.2f\n",taxes);
+	printf("Net pay:$%.2f\n",netPay);
+	
+	
+	
+	
+	
+	return 0;
+} 
